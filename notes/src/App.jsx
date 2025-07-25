5c7b7110d2a177cdf0cb68765410418410ca8d46
Alex Bilba <54191007+alexshanless@users.noreply.github.com>
2025-07-25 11:04:35 -0700

    submit

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import NotesForm from './components/NoteForm';
+import NotesList from './components/NotesList';
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
   return (
-    <div className='max=w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg'>
+    <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg'>
       <h2 className='text-2xl font-bold mb-4 text-center'>Notes App</h2>
-      <NotesForm />
+      <NotesForm notes={notes} setNotes={setNotes} />
+      <NotesList notes={notes} />
     </div>
   );
 };
--- notes/src/components/NoteForm.jsx
+++ notes/src/components/NoteForm.jsx
@@ -1,76 +1,113 @@
 import { useState } from 'react';
 
-const NotesForm = () => {
-  const [title, setTitle] = useState('');
-  const [priority, setPriority] = useState('Medium');
-  const [category, setCategory] = useState('Work');
-  const [description, setDescription] = useState('');
+const NotesForm = ({ notes, setNotes }) => {
+  const [formData, setFormData] = useState({
+    title: '',
+    priority: 'Medium',
+    category: 'Work',
+    description: '',
+  });
+
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleSumbit = e => {
+    e.preventDefault();
+    if (!formData.title) return;
+    // Call the function to add a new note
+    const newNote = { id: Date.now(), ...formData };
+    setNotes([newNote, ...notes]);
+    // Reset the form
+    setFormData({
+      title: '',
+      priority: 'Medium',
+      category: 'Work',
+      description: '',
+    });
+  };
 
   return (
-    <form className='mb-6'>
-      <div className='mb-4'>
-        <label htmlFor='title' className='block font-semibold'>
-          Title
-        </label>
-        <input
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-        />
-        {title}
-      </div>
-      <div className='mb-4'>
-        <label htmlFor='priority' className='block font-semibold'>
-          Priority
-        </label>
-        <select
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={priority}
-          onChange={e => setPriority(e.target.value)}
-        >
-          <option value='High'>High</option>
-          <option value='Medium'>Medium</option>
-          <option value='Low'>Low</option>
-        </select>
-        {priority}
-      </div>
-      <div className='mb-4'>
-        <label htmlFor='category' className='block font-semibold'>
-          Category
-        </label>
-        <select
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={category}
-          onChange={e => setCategory(e.target.value)}
-        >
-          <option value='Work'>Work</option>
-          <option value='Personal'>Personal</option>
-          <option value='Ideas'>Ideas</option>
-        </select>
-        {category}
-      </div>
-      <div className='mb-4'>
-        <label htmlFor='description' className='block font-semibold'>
-          Description
-        </label>
-        <textarea
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-        ></textarea>
-        {description}
-      </div>
+    <>
       <button
-        type='submit'
-        className='w-full bg-purple-500 text-white py-2 rounded-lg hover:bg-blue-600'
+        onClick={() => setIsFormVisible(!isFormVisible)}
+        className='w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-200 hover:border-pruple-300 transition mb-4'
       >
-        Add Note
+        {isFormVisible ? 'Hide Form' : 'Add new Note'}
       </button>
-    </form>
+      {isFormVisible && (
+        <form onSubmit={handleSumbit} className='mb-6'>
+          <div className='mb-4'>
+            <label htmlFor='title' className='block font-semibold'>
+              Title
+            </label>
+            <input
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.title}
+              onChange={e =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+            />
+            {formData.title}
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='priority' className='block font-semibold'>
+              Priority
+            </label>
+            <select
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.priority}
+              onChange={e =>
+                setFormData({ ...formData, priority: e.target.value })
+              }
+            >
+              <option value='High'>High</option>
+              <option value='Medium'>Medium</option>
+              <option value='Low'>Low</option>
+            </select>
+            {formData.priority}
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='category' className='block font-semibold'>
+              Category
+            </label>
+            <select
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.category}
+              onChange={e =>
+                setFormData({ ...formData, category: e.target.value })
+              }
+            >
+              <option value='Work'>Work</option>
+              <option value='Personal'>Personal</option>
+              <option value='Ideas'>Ideas</option>
+            </select>
+            {formData.category}
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='description' className='block font-semibold'>
+              Description
+            </label>
+            <textarea
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              value={formData.description}
+              onChange={e =>
+                setFormData({ ...formData, description: e.target.value })
+              }
+            ></textarea>
+            {formData.description}
+          </div>
+          <button
+            type='submit'
+            className='w-full bg-purple-500 text-white py-2 rounded-lg hover:bg-blue-600'
+          >
+            Add Note
+          </button>
+        </form>
+      )}
+    </>
   );
 };
 
