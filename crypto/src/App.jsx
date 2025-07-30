7d8b739ad7565fa2b98b147d21c17ad515b94e4b
Alex Bilba <alex.shanless@gmail.com>
2025-07-30 12:05:51 -0700

    cards setup

--- crypto/src/App.jsx
+++ crypto/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, use } from 'react';
+import CoinCard from './components/CoinCard';
 const API_URL =
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
@@ -16,6 +17,7 @@ const App = () => {
         }
         const data = await res.json();
         console.log(data);
+        setCoins(data);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -27,6 +29,15 @@ const App = () => {
   return (
     <div>
       <h1> Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className='error'>Error: {error}</div>}
+      {!loading && !error && (
+        <main className='grid'>
+          {coins.map(coin => (
+            <CoinCard key={coin.id} coin={coin} />
+          ))}
+        </main>
+      )}
     </div>
   );
 };
